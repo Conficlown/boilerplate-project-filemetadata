963eafa89ae3ac8ed036169ea137c84bab7a1888
Glitch (brave-tuck) <none>
2019-11-14 21:57:37 +0000

    🛍😼 Updated with Glitch

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+
+// SET STORAGE
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + '/public');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  }
+})
+ 
+var upload = multer({ storage: storage });
 
 var app = express();
 
@@ -16,6 +29,17 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next)=>{
+  const file = req.file;
+  if (!file) {
+    const error = new Error('Please upload a file');
+    error.httpStatusCode = 400;
+    return next(error);
+  }
+  console.log(file);
+  res.json({filename: file.filename, size: file.size});
 });
 
 app.listen(process.env.PORT || 3000, function () {
